dfbaf9bd7a1c24039368b7f277929d8b14672322
Gary Marroquin <gary.marroquin@gmail.com>
2017-10-16 19:17:19 -0500

    Nick's Updated Js

--- assets/script/goingout.js
+++ assets/script/goingout.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
             var firstPic = $("<div>");
 
             //functions to place restaurant information in their respective div
-            restaurantsdiv1.html(data.restaurants[11].restaurant.name);
+            restaurantsdiv1.html(data.restaurants[5].restaurant.name);
             console.log(data.restaurants[5].restaurant.name)
             $("#restaurantsdiv1").html(restaurantsdiv1);
-            restaurantsdiv2.html(data.restaurants[8].restaurant.name);
+            restaurantsdiv2.html(data.restaurants[6].restaurant.name);
             $("#restaurantsdiv2").html(restaurantsdiv2);
-            restaurantsdiv3.html(data.restaurants[2].restaurant.name);
+            restaurantsdiv3.html(data.restaurants[3].restaurant.name);
             $("#restaurantsdiv3").html(restaurantsdiv3);
-            restaurantsdiv4.html(data.restaurants[3].restaurant.name);
+            restaurantsdiv4.html(data.restaurants[10].restaurant.name);
             $("#restaurantsdiv4").html(restaurantsdiv4);
 
             firstPic.html(data.restaurants[5].restaurant.featured_image);
@@ -34,41 +34,60 @@ $(document).ready(function() {
             $("#firstPic").html(firstPic);
         });
     });
-});
+
 
 // Nick's javascript
-$('#mainbutton').on('click', function() {
-    var recipe = $(this).data('name');
-    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=4&tags=";
+     $('#mainbutton').on('click', function() {
 
+    var recipe = $(this).data('name');
+
+    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=4&tags=";
     $.ajax({
         url: queryURL,
-        headers: {
-            'X-Mashape-Key': 'QjnzWlmOacmshwsPibENUtYYmB3Zp1X16Xmjsn1sIUIJeoo2Gx'
-        },
+        headers: { 'X-Mashape-Key': 'QjnzWlmOacmshwsPibENUtYYmB3Zp1X16Xmjsn1sIUIJeoo2Gx' },
         method: 'GET'
-
-    }).done(function(response) {
-        displayRecipes(response.recipes)
+    })
+        .done(function(response) {
+            displayRecipes(response.recipes);
+            displayImages(response.recipes);
+            giveUrl(response.recipes);
+            $("#img1").css("background","#fff8d3");
+            $("#img2").css("background","#fff8d3");
+            $("#img3").css("background","#fff8d3");
+            $("#img4").css("background","#fff8d3");
+            
+        });
     });
-});
 
-
-function displayRecipes(recipes) {
+function displayRecipes(recipes){
     console.log(recipes);
-    var recipediv1 = $("<div>")
+    var recipediv1 = $("<div>") 
     recipediv1.html(recipes[0].title)
     $("#recipediv1").html(recipediv1)
-    var recipediv2 = $("<div>")
+    var recipediv2 = $("<div>") 
     recipediv2.html(recipes[1].title)
     $("#recipediv2").html(recipediv2)
-    var recipediv3 = $("<div>")
+    var recipediv3 = $("<div>") 
     recipediv3.html(recipes[2].title)
     $("#recipediv3").html(recipediv3)
-    var recipediv4 = $("<div>")
+    var recipediv4 = $("<div>") 
     recipediv4.html(recipes[3].title)
     $("#recipediv4").html(recipediv4)
-}
+  }
+
+function displayImages(recipes){
+    $("#img1").html("<img src='" + recipes[0].image + "' alt='recipeimage' style='height:220px; width:370px'>")
+    $("#img2").html("<img src='" + recipes[1].image + "' alt='recipeimage' style='height:220px; width:370px'>")
+    $("#img3").html("<img src='" + recipes[2].image + "' alt='recipeimage' style='height:220px; width:370px'>")
+    $("#img4").html("<img src='" + recipes[3].image + "' alt='recipeimage' style='height:220px; width:370px'>")
+  }
+
+ function giveUrl(recipes){
+    $("#link1").attr("href",recipes[0].spoonacularSourceUrl)
+    $("#link2").attr("href",recipes[1].spoonacularSourceUrl)
+    $("#link3").attr("href",recipes[2].spoonacularSourceUrl)
+    $("#link4").attr("href",recipes[3].spoonacularSourceUrl)
+ }
 
 
-
+});
